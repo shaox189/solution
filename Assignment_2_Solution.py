18f9d17364ddd6da7fb3ae2098d8d05611e4ad3c
shaox189 <99113424+shaox189@users.noreply.github.com>
2022-03-10 21:50:13 -0600

    Add files via upload

---
+++ Assignment_2_Solution.py
@@ -0,0 +1,43 @@
+{
+    "metadata": {
+        "kernelspec": {
+            "name": "python3",
+            "display_name": "Python 3 (ipykernel)",
+            "language": "python"
+        },
+        "language_info": {
+            "name": "python",
+            "version": "3.8.10",
+            "mimetype": "text/x-python",
+            "codemirror_mode": {
+                "name": "ipython",
+                "version": 3
+            },
+            "pygments_lexer": "ipython3",
+            "nbconvert_exporter": "python",
+            "file_extension": ".py"
+        }
+    },
+    "nbformat_minor": 2,
+    "nbformat": 4,
+    "cells": [
+        {
+            "cell_type": "code",
+            "source": [
+                "import numpy as np\r\n",
+                "import math\r\n",
+                "\r\n",
+                "S = 100\r\n",
+                "K = 100\r\n",
+                "T = 1\r\n",
+                "r = 0.05\r\n",
+                "sig = 0.4"
+            ],
+            "metadata": {
+                "azdata_cell_guid": "fd088c81-b07f-41fc-b49a-0fa9d56375b5"
+            },
+            "outputs": [],
+            "execution_count": null
+        }
+    ]
+}
